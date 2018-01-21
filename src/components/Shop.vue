789411458dd6e193faffb98b50a8d88882073691
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-21 15:26:22 -0300

    Add shop component

---
+++ src/components/Shop.vue
@@ -0,0 +1,31 @@
+<template>
+  <md-list>
+    <md-list-item v-for="item in items" v-bind:key="item.itemId">
+      <img class="item-icon" :src="fetchIconSrc(itemId)">
+      <span class="md-list-item-text">{{ item.name }}</span>
+    </md-list-item>
+  </md-list>
+</template>
+<script>
+export default {
+  name: 'item-shop',
+  computed: {
+    // item() {
+    //   if (this.$route.params)
+    // }
+  },
+  methods: {
+    fetchIconSrc (itemId) {
+      switch (itemId) {
+
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.shop-icon {
+
+}
+</style>
