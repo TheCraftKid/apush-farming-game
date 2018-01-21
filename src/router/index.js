4ab5fda16f39d851e87f8dc218fe7e48cf7ed9cb
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-21 14:03:56 -0300

    Update router to include new components

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Shop from '@/components/Shop'
+import Leaderboard from '@/components/Leaderboard'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/start',
+      name: 'Start Screen'
+    },
+    {
+      path: '/shop/:item_id',
+      name: 'Store',
+      component: Shop
+    },
+    {
+      path: '/leaderboard',
+      name: 'Leaderboard',
+      component: Leaderboard
     }
   ]
 })
