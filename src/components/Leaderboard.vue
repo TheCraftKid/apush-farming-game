0a14c356b8f0d586038e605bb183b22d8da1abf2
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-20 23:15:00 -0300

    Create leaderboard

---
+++ src/components/Leaderboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <md-table md-card v-model="scores">
+    <md-table-toolbar>
+      <h1 class="md-title">High Scores</h1>
+    </md-table-toolbar>
+    <md-table-row slot="md-table-row" slot-scope="{ item }">
+      <md-table-cell md-label="Score" md-sort-by="score" md-numeric>{{ item.score }}</md-table-cell>
+      <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
+    </md-table-row>
+  </md-table>
+</template>
+
+<script>
+import * as firebase from 'firebase'
+
+export default {
+  name: 'game-leaderboard',
+  computed: {
+    scores () {
+      
+    }
+  }
+}
+</script>
