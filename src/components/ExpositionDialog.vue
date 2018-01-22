069c20b20708b66cd05a59e2412c011e720caaf9
Willie Chalmers III <willieechalmers@gmail.com>
2018-01-21 21:55:28 -0300

    Add exposition dialog to explain events to user

---
+++ src/components/ExpositionDialog.vue
@@ -0,0 +1,33 @@
+<template>
+  <md-card>
+    <md-card-header>
+     <h2 class="md-title">{{ title }}</h2>
+     <div class="md-subhead">{{ subtitle }}</div>
+    </md-card-header>
+
+    <md-card-content>{{ exposition }}</md-card-content>
+    
+    <md-card-actions>
+      <md-button v-on:click="triggerCallback">{{ actionText }}</md-button>
+    </md-card-actions>
+  </md-card>
+</template>
+
+<script>
+export default {
+  name: 'exposition-dialog',
+  props: ['title', 'subtitle', 'exposition', 'actionText'],
+  methods: {
+    triggerCallback (event) {
+      this.$emit('on-action')
+    }
+  }
+}
+</script>
+
+<style>
+md-card {
+  max-width: 320px;
+  margin: 16px;
+}
+</style>
